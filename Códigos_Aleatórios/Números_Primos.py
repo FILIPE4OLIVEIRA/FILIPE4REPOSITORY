2d63be260ede1436f4ec5838ede12fed0ac33fa1
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2020-02-28 02:59:22 -0300

    Update Números_Primos.py

--- Códigos_Aleatórios/Números_Primos.py
+++ Códigos_Aleatórios/Números_Primos.py
@@ -7,33 +7,46 @@ Created on Sat Sep 28 22:38:09 2019
 
 # Lista de Números Primos
 
-import os
+import numpy
+import matplotlib.pyplot as plt
 
-numb = int(input("digite um número limite: "))
+Numb = int(input("digite um número limite: "))
 
-odd_numb = []       # impares
-even_numb = []      # pares
-prime_numb = [2]    # primos
+def Numeros_Primos(Numb):
+    
+    odd_numb = []       # impares
+    even_numb = []      # pares
+    prime_numb = [2]    # primos
 
-for x in range(2, numb + 1):                # Cria um lista de números Pares e Impares
-    if (x % 2 == 0):
-        even_numb.append(x)
-    else:
-        odd_numb.append(x)
+    for x in range(2, Numb + 1):                # Cria um lista de números Pares e Impares
+        if (x % 2 == 0):
+            even_numb.append(x)
+        else:
+            odd_numb.append(x)
 
-for x in range(2, len(odd_numb) + 1):
-    for y in range(3, len(odd_numb) + 1):  # Remove todos os múltiplos impares da lista
-        z = y * x
-        if (z in odd_numb):
-            odd_numb.remove(z)
+    for x in range(2, len(odd_numb) + 1):
+        for y in range(3, len(odd_numb) + 1):  # Remove todos os múltiplos impares da lista
+            z = y * x
+            if (z in odd_numb):
+                odd_numb.remove(z)
 
-for x in odd_numb:                         # Monta o vetor contendo todos os números com resto da divisão igual a zero
-    for y in range(3, numb + 1):
-        if (x % y == 0):
-            prime_numb.append(x)
+    for x in odd_numb:                         # Monta o vetor contendo todos os números com resto da divisão igual a zero
+        for y in range(3, Numb + 1):
+            if (x % y == 0):
+                prime_numb.append(x)
 
-print("\n São %.i Números Primos até o %.i" % (len(prime_numb),numb))
-print(" A lista de numeros primos é: ", str(prime_numb))
-print("\n")
+    P1 = print("\n São %.i Números Primos até o %.i" % (len(prime_numb),Numb))
+    P2 = print(" A lista de numeros primos é: ", str(prime_numb))
 
-os.system("pause")
+    # Plotagem do Gráfico dos Números Primos 
+    plt.title('Gráfico dos Números Primos')
+    plt.xlabel('EIXO X')
+    plt.ylabel('EIXO Y')
+    plt.plot(prime_numb, marker = 'H' , label = 'Números Primos', color = 'red', linestyle = 'dotted')
+    plt.legend(loc = 'best')
+    plt.grid()
+    plt.show()
+
+    return(P1,P2)
+
+Numeros_Primos(Numb)
