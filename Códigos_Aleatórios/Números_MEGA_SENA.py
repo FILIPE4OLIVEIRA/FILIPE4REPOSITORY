8f8a729ee0394af71a6bf54f95f3c6b40f86a5b9
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2022-12-06 19:02:43 -0300

    Update Números_MEGA_SENA.py

--- Códigos_Aleatórios/Números_MEGA_SENA.py
+++ Códigos_Aleatórios/Números_MEGA_SENA.py
@@ -2,37 +2,44 @@
 """
 Created on Sat Jan 15 22:38:09 2020
 @author: engoliveira
-
 """
 
 # Sorteio de Números da Mega Sena
 
 import random
-import os
 
-def numeros_mega(h):
-	numb = []
-	for i in range(h):
-		numb.clear()
-		while(len(numb)<6):
-			X = random.randint(1,60)
-			if(X in numb):
-				numb.remove(X)
-			else:
-				numb.append(X)
 
-		numb.sort()
+def numeros_mega(h=30):
+    numb = []
+    AllNumbs = []
+    for i in range(h):
+        numb.clear()
+        while(len(numb)<6):
+            X = random.randint(1,60)
+            if(X in numb):
+                numb.remove(X)
+            else:
+                numb.append(X)
+                
+        numb.sort()
+        
+        for j in range(6):
+            if(numb[j]<10):
+                numb[j] = str("0" + str(numb[j]))
+            else:
+                numb[j] = str(numb[j])
+        
+        Numb2 = numb.copy()
+        AllNumbs.append(Numb2)
+        print(numb)
+    
+    return(AllNumbs)
 
-		for j in range(6):
-			if(numb[j]<10):
-				numb[j] = str("0" + str(numb[j]))
-			else:
-				numb[j] = str(numb[j])
+AllNumbs = numeros_mega(int(input("Quantos Sorteios Deseja Realizar? ")))
 
-		print(numb)	
-
-	return(numb)
-
-numeros_mega(int(input("Quantos Sorteios Deseja Realizar? ")))
-
-os.system("pause")
+ArquivoResult = open(r"C:/PythonCodes/JogosMega.txt","a")
+ArquivoResult = open(r"C:/PythonCodes/JogosMega.txt","w+")
+for i in range(len(AllNumbs)):
+    ArquivoResult.writelines(str(AllNumbs[i]) + "\n")
+ArquivoResult.close()
+print("FINISH!")
