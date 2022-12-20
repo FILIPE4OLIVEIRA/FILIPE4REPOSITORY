3732438ad5108b04e65a6a26cf37249d9d840460
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2022-12-20 09:50:11 -0300

    Update Números_MEGA_SENA.py

--- Códigos_Aleatórios/Números_MEGA_SENA.py
+++ Códigos_Aleatórios/Números_MEGA_SENA.py
@@ -3,43 +3,49 @@
 Created on Sat Jan 15 22:38:09 2020
 @author: engoliveira
 """
-
 # Sorteio de Números da Mega Sena
 
 import random
 
-
 def numeros_mega(h=10):
-    numb = []
-    AllNumbs = []
-    for i in range(h):
-        numb.clear()
-        while(len(numb)<6):
-            X = random.randint(1,60)
-            if(X in numb):
-                numb.remove(X)
-            else:
-                numb.append(X)
-                
-        numb.sort()
-        
-        for j in range(6):
-            if(numb[j]<10):
-                numb[j] = str("0" + str(numb[j]))
-            else:
-                numb[j] = str(numb[j])
-        
-        Numb2 = numb.copy()
-        AllNumbs.append(Numb2)
-        print(numb)
-    
-    return(AllNumbs)
+	lista_numeros = []
+	lista_jogos = []
+	for i in range(h):
+		lista_numeros.clear()
+		while(len(lista_numeros)<6):
+			X = random.randint(1,60)
+			if(X not in lista_numeros):
+				lista_numeros.append(X)
 
-AllNumbs = numeros_mega(int(input("Quantos Sorteios Deseja Realizar? ")))
+		lista_numeros.sort()
+		for j in range(6):
+			if(lista_numeros[j]<10):
+				lista_numeros[j] = str("0" + str(lista_numeros[j]))
+			else:
+				lista_numeros[j] = str(lista_numeros[j])
+		
+		lista_string = lista_numeros.copy()
+		lista_jogos.append(lista_string)
+		lista_jogos.append(lista_string)
+	
+	index = 0
+	while(lista_jogos[index] in lista_jogos):
+			lista_jogos.remove(lista_jogos[index])
+			if(index + 1 < len(lista_jogos)):
+				index = index + 1
+			else:
+				break
 
-ArquivoResult = open(r"C:/PythonCodes/JogosMega.txt","a")
-ArquivoResult = open(r"C:/PythonCodes/JogosMega.txt","w+")
-for i in range(len(AllNumbs)):
-    ArquivoResult.writelines(str(AllNumbs[i]) + "\n")
-ArquivoResult.close()
-print("FINISH!")
+	return(lista_jogos)
+
+def grava_dados(lista_jogos):
+	ArquivoResult = open(r"C:/Users/filipe.cardoso/Desktop/JogosMega.txt", "a")
+	ArquivoResult = open(r"C:/Users/filipe.cardoso/Desktop/JogosMega.txt","w+")
+	for i in range(len(lista_jogos)):
+		ArquivoResult.writelines(str(lista_jogos[i]) + "\n")
+	ArquivoResult.close()
+
+
+lista_jogos = numeros_mega(int(input("Quantos Sorteios Deseja Realizar? ")))
+grava_dados(lista_jogos)
+
