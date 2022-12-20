0877a22734e52904c0169499bde93ef8dd4a5cec
FILIPE OLIVEIRA <55719753+FILIPE4OLIVEIRA@users.noreply.github.com>
2022-12-20 17:43:59 -0300

    Update Números_Primos.py

--- Códigos_Aleatórios/Números_Primos.py
+++ Códigos_Aleatórios/Números_Primos.py
@@ -7,28 +7,26 @@ Created on Sat Sep 28 22:38:09 2019
 
 # Lista de Números Primos
 
-numb = int(input("Digite um Número Limite: "))
+lista_primos = int(input("Digite um Número Limite: "))
 
-odd_numb = []       # impares
-even_numb = []      # pares
-prime_numb = [2]    # primos
 
-for x in range(2, numb + 1):                # Cria um lista de números Pares e Impares
-    if (x % 2 == 0):
-        even_numb.append(x)
-    else:
-        odd_numb.append(x)
+odd_numbers = []       # impares
+prime_numbers = [2]    # primos
 
-for x in range(2, len(odd_numb) + 1):
-    for y in range(3, len(odd_numb) + 1):  # Remove todos os múltiplos impares da lista
+for x in range(2, lista_primos + 1):                # Cria um lista de números Pares e Impares
+    if (x % 2 != 0):
+        odd_numbers.append(x)
+
+for x in range(2, len(odd_numbers) + 1):
+    for y in range(3, len(odd_numbers) + 1):  # Remove todos os múltiplos impares da lista
         z = y * x
-        if (z in odd_numb):
-            odd_numb.remove(z)
+        if (z in odd_numbers):
+            odd_numbers.remove(z)
 
-for x in odd_numb:                         # Monta o vetor contendo todos os números com resto da divisão igual a zero
-    for y in range(3, numb + 1):
+for x in odd_numbers:                   # Monta o vetor contendo todos os números
+    for y in range(3, lista_primos + 1):        # com resto da divisão igual a zero
         if (x % y == 0):
-            prime_numb.append(x)
+            prime_numbers.append(x)
 
-print("\n São %.i Números Primos até o Número %.i" % (len(prime_numb),numb))
-print("\n A Lista de Números Primos é: ", str(prime_numb))
+print("\n São %.i Números Primos até o Número %.i" % (len(prime_numbers),lista_primos))
+print("\n A Lista de Números Primos é: ", str(prime_numbers))
